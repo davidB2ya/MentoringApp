70584b557dc021cd48a30ffa6827261251e4e37a
angelicahg <herrerangelik@gmail.com>
2021-11-08 19:48:19 -0500

    test changes

--- api/src/db/mongo.js
+++ api/src/db/mongo.js
@@ -25,3 +25,21 @@ process.on('uncaughtException', error => {
   console.error(error)
   mongoose.disconnect()
 })
+
+/* const mongoose = require('mongoose');
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+
+    console.log('DB online');
+    console.log(error);
+    throw new Error('Error en la base de datos - vea logs')
+  }
+}
+module.exports = {
+  dbConnection
+} */
