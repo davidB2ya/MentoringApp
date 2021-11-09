3db109c812ad760f6efe5dfc6e9ca9bc87aa4c77
Sebastian Ocampo <sebastianocampolopez507@gmail.com>
2021-11-09 14:26:35 -0500

    database modeling

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
-// const usersRouter = require('../controllers/users')
+const pruebaRouter = require('../controllers/login')
 // const loginRouter = require('../controllers/login')
 
 const router = Router()
 
-// router.use('/api/users', usersRouter)
+router.use('/api/prueba', pruebaRouter)
 
 // router.use('/api/login', loginRouter)
 
