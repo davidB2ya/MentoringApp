a39cca112d20fa1ed88a60a5530b1bcb6b9d2ced
daferrom <daferrom@gmail.com>
2021-11-12 15:53:27 -0500

    initial point routing develop in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,60 +5,12 @@ import loginService from './services/login'
 import CrudStudent from'./views/Administrator/Cruds/students/index'
 
 function App() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
-
-  const [errorMessage, setErrorMessage] = useState(null)
-
-
-  const handleLogin = async (event) => {
-    event.preventDefault()
-
-    try {
-      const user = await loginService.login({
-        username,
-        password
-      })
-
-      window.localStorage.setItem(
-        'loggedNoteAppUser', JSON.stringify(user)
-      )
-
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (e) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-  }
+  
 
   return (
     <div className="App">
-      {/*
-      <Notification message={errorMessage} />
-        {
-        user
-          ? <h1>Existe usuario</h1>
-          : <LoginForm
-              username={username}
-              password={password}
-              handleUsernameChange={
-                ({ target }) => setUsername(target.value)
-              }
-              handlePasswordChange={
-                ({ target }) => setPassword(target.value)
-              }
-              handleSubmit={handleLogin}
-            />
-            
-      }*/}
 
       <CrudStudent/>
-
 
     </div>
   );
