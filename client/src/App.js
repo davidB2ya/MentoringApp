b0cacc055c55c9622fc408a209b28d9dbb1f1bd5
daferrom <daferrom@gmail.com>
2021-11-15 18:58:19 -0500

    routing config in react in progress

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,27 @@ import { useState } from "react"
 import LoginForm from "./components/LoginForm"
 import Notification from "./components/Notification"
 import loginService from './services/login'
-import CrudStudent from'./views/Administrator/Cruds/students/index'
-import Navbar from './components/Navbar/Navbar'
+//import CrudStudent from'./views/Administrator/Cruds/students/index'
+//import Navbar from './components/Navbar/Navbar'
+import { Link } from "react-router-dom"
+
+
 function App() {
   
 
   return (
     <div className="App">
-
-      <CrudStudent/>
-
+      <h1>Testing Router React</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+      {/*<CrudStudent/>*/}
+      <Link to="/welcome-user"></Link>
+      <Link to="/welcome-student"></Link>
+      </nav>
     </div>
   );
 }
