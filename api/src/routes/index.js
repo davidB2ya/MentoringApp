7c0cda84958a599b9e1456c09e21847db94eb68d
davidB2ya <dbedoya0408@gmail.com>
2021-11-19 14:14:35 -0500

    create a controller for edit profile

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -18,7 +18,9 @@ const answerBankRouter = require('../controllers/answerBank')
 
 const questionBankRouter = require('../controllers/questionBank')
 
-// const notifMentorRouter = require('../controllers/notif-mentor')
+const profilEditRouter = require('../controllers/profilEdit')
+
+const notifMentorRouter = require('../controllers/notif-mentor')
 
 const router = Router()
 
@@ -38,13 +40,16 @@ router.use('/api/students-control-update', StudentsRouter.updatedUserRouter)
 
 router.use('/api/students-control-post', StudentsRouter.postUserRouter)
 
-router.use('/api/studentsPerfil-control-update', StudentsRouter.updatedProfileRouter)
+router.use(
+  '/api/studentsPerfil-control-update',
+  StudentsRouter.updatedProfileRouter
+)
 
 router.use('/api/session', sessionRouter)
 
 router.use('/api/user', userRouter)
 
-router.use('/api/dashboard/users', dashBoardRouter.userRouter)
+router.use('/api/dashboard', dashBoardRouter.userRouter)
 
 router.use('/api/dashboard/answer', dashBoardRouter.answerRouter)
 
@@ -53,6 +58,13 @@ router.use('/api/answerBank', answerBankRouter)
 router.use('/api/questionBank', questionBankRouter)
 
 router.use('/api/dashboard/assignedsession', dashBoardRouter.assigSessionRouter)
-// router.use('/api/notif-mentor', notifMentorRouter)
+
+router.use('/api/dashboard/infoStudent', dashBoardRouter.infoStudentRouter)
+
+router.use('/api/profile-edit', profilEditRouter.profilEditRouter)
+
+router.use('/api/updated-profile-edit', profilEditRouter.updatedprofilEditRouter)
+
+router.use('/api/notif-mentor', notifMentorRouter)
 
 module.exports = router
