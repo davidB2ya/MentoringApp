580a13831c32e8d667a622ed5e34449d03884767
daferrom <daferrom@gmail.com>
2021-11-19 07:20:40 -0500

    alt text added to images inside footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -15,22 +15,22 @@ const Footer = () => {
     <section className="footer-social-media">
       
     <a href="https://www.facebook.com/somoseducamas" target="_blank" rel="noopener noreferrer">
-      <img className="facebook" src={Facebook} width="20px" height="20px"/></a>
+      <img className="facebook" src={Facebook} width="20px" height="20px" alt="Facebook"/></a>
       <a href="https://www.instagram.com/programate_escueladecodigo/" target="_blank" rel="noopener noreferrer">
-      <img className="instagram"src={Instagram} width="20px" height="20px"/></a>
+      <img className="instagram"src={Instagram} width="20px" height="20px" alt="Instagram"/></a>
       <a href="https://www.youtube.com/channel/UCmnr_sLPZ1E8H1VgUtaHGPQ" target="_blank" rel="noopener noreferrer">
-      <img className="youtube"src={Youtube} width="20px" height="20px"/></a>
+      <img className="youtube"src={Youtube} width="20px" height="20px" alt="Youtube"/></a>
       
     </section>
     <section className="footer-info">
       <section className="img">
         <section className="footer-info__name">
-        <img src={Programate} width="120px" height="30px"/>
+        <img src={Programate} width="120px" height="30px" alt="Programate Logo"/>
         </section>
             
       </section>
       <section className="footer-info-right">
-      <img src={Educamas} width="120px" height="30px"/>
+      <img src={Educamas} width="120px" height="30px" alt="Educamás Logo"/>
         
        
       </section>
