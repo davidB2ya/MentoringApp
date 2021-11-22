896c7a90563126cf7777906f9e5805025e24a8cb
daferrom <daferrom@gmail.com>
2021-11-22 17:49:18 -0500

    checkpoint 5:49 22 nov

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ import ForgotPassword from './components/login/ForgotPassword.jsx'
 //others
 import WelcomeUser from './views/Student/Welcome/WelcomeStudent.jsx'
 import WelcomeStudent from './views/Student/Welcome/WelcomeStudent' 
-import StudentSession from './views/Student/SessionsBoard/SessionsBoard';
+import StudentSession from './views/Student/SessionsBoard/sessionsBoard';
 import Thanks from './views/Student/Thanks/Thanks';
 import NavBar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
@@ -60,7 +60,7 @@ function UserLogged (){
          {/* <Route path= '/forgot_password' element={<ForgotPassword/>} exact/> */}
  
          {/* others */}
-         <Route path="/" element={<PrincipalView/>}/>
+         <Route path="/home" element={<PrincipalView/>}/>
          <Route path="/welcome-user" element={<WelcomeUser/>}/>
          <Route path="/welcome-student" element={<WelcomeStudent/>}/>
          <Route path="/form-student/:id" element={<FirstStudentForm/>}/>
@@ -115,8 +115,10 @@ function App() {
 
   return(
     <>
-      { userLogged ? <UserLogged/> : <UserNotLogged/> }
+     { userLogged ? <UserLogged/> : <UserNotLogged/> }
     </>
+    
+
   ) 
   
   
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -8,7 +8,8 @@ import {
 import { dispatchLogin } from '../../redux/actions/authActions'
 import { useDispatch } from 'react-redux'
 import './login.css'
-
+import logo from '../../assets/Logo/programateLogo.png'
+import Input from '../Input/Input'
 
 
 const Login = () => {
@@ -25,9 +26,10 @@ const Login = () => {
   const navigate = useNavigate() 
 
   const { email, password, err, success } = user
-
+  //console.log(user)
   const handleChangeInput = e => {
     const { name, value } = e.target
+    //console.log(name, value)
     setUser({ ...user, [name]: value, err: '', success: '' })
   }
 
@@ -38,14 +40,14 @@ const Login = () => {
         email,
         password
       })
-      
+      console.log(res)
       setUser({ ...user, err: '', success: res.data.msg })
       window.localStorage.setItem('firstLogin', true)
       window.localStorage.setItem(
         'loggedAgoraUser', JSON.stringify(res.data)
       )
       dispatch(dispatchLogin())
-      navigate('/')
+      navigate('/home')
     } catch (err) {
       err.response.data.error &&
         setUser({ ...user, err: err.response.data.error, success: '' })
@@ -53,45 +55,44 @@ const Login = () => {
   }
 
   return (
-    <div className='login_page'>
-      <h2>Login</h2>
+    <div className='container-login-main'>
+      <div className='container-login-page'>
+      <img className='logo' src={logo} alt='logo-programate' />
+      <h2>Ingresa a Okhos</h2>
       {err && showErrMsg(err)}
       {success && showSuccessMsg(success)}
 
-      <form onSubmit={handleSubmit}>
-        <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='container-login-form-content'>
           <label htmlFor='email'>Email Address</label>
-          <input
-            type='text'
-            placeholder='Enter email address'
-            id='email'
-            value={email}
-            name='email'
-            onChange={handleChangeInput}
-          />
+          <Input
+              label='Correo'
+              placeholder='Luis@hotmail.com'
+              name='email'
+              value={email}
+              onChange={handleChangeInput}
+            />
+        
+        <Input
+              type='Password'
+              label='Contraseña'
+              placeholder='********'
+              name='password'
+              value={password}
+              onChange={handleChangeInput}
+            />
         </div>
-
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            placeholder='Enter password'
-            id='password'
-            value={password}
-            name='password'
-            onChange={handleChangeInput}
-          />
-        </div>
-
-        <div className='row'>
-          <button type='submit'>Login</button>
-          <Link to='/forgot_password'>Forgot your password?</Link>
-        </div>
+        
+        <button className='button-login' type='submit'>
+            INGRESAR
+          </button>
+        
       </form>
 
       <p>
         New Customer? <Link to='/register'>Register</Link>
       </p>
+      </div>
     </div>
   )
 }
