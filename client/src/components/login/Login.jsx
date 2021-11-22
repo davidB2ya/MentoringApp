eb7fbe2cc8a37d40bcfaf16ff7ce3a9344eceae1
Sebastian Ocampo <sebastianocampolopez507@gmail.com>
2021-11-22 16:52:08 -0500

    updated login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -6,12 +6,15 @@ import {
     showSuccessMsg
   } from '../notification/Notification'
 import { dispatchLogin } from '../../redux/actions/authActions'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './login.css'
-
+import Input from './Input'
+import logo from '../assets/images/programate-fblanco.png'
 
 
 const Login = () => {
+
+  
 
   //Inicializo hooks
   const [user, setUser] = useState({
@@ -25,6 +28,7 @@ const Login = () => {
   const navigate = useNavigate() 
 
   const { email, password, err, success } = user
+
 
   const handleChangeInput = e => {
     const { name, value } = e.target
@@ -45,7 +49,11 @@ const Login = () => {
         'loggedAgoraUser', JSON.stringify(res.data)
       )
       dispatch(dispatchLogin())
-      navigate('/')
+
+      
+
+      
+      navigate('/welcome-user')
     } catch (err) {
       err.response.data.error &&
         setUser({ ...user, err: err.response.data.error, success: '' })
@@ -53,45 +61,44 @@ const Login = () => {
   }
 
   return (
-    <div className='login_page'>
-      <h2>Login</h2>
+    <div className='container-login-main'>
+      <div className='container-login-page'>
+      <img className='logo' src={logo} alt='logo-programate' />
+      <h2>Ingresa a Okhos</h2>
       {err && showErrMsg(err)}
       {success && showSuccessMsg(success)}
 
-      <form onSubmit={handleSubmit}>
-        <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='container-login-form-content'>
           <label htmlFor='email'>Email Address</label>
-          <input
-            type='text'
-            placeholder='Enter email address'
-            id='email'
-            value={email}
-            name='email'
-            onChange={handleChangeInput}
-          />
+          <Input
+              label='Correo'
+              placeholder='Luis@hotmail.com'
+              name='email'
+              value={email}
+              onChange={handleChangeInput}
+            />
+        
+        <Input
+              type='Password'
+              label='Contraseña'
+              placeholder='********'
+              name='password'
+              value={password}
+              onChange={handleChangeInput}
+            />
         </div>
-
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            placeholder='Enter password'
-            id='password'
-            value={password}
-            name='password'
-            onChange={handleChangeInput}
-          />
-        </div>
-
-        <div className='row'>
-          <button type='submit'>Login</button>
-          <Link to='/forgot_password'>Forgot your password?</Link>
-        </div>
+        
+        <button className='button-login' type='submit'>
+            INGRESAR
+          </button>
+        
       </form>
 
       <p>
         New Customer? <Link to='/register'>Register</Link>
       </p>
+      </div>
     </div>
   )
 }
