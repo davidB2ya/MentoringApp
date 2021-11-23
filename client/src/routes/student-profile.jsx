5ae21e5de734deb7c46ab13243e5ec55305ba8e5
Camilomorah <cami88mh_@hotmail.com>
2021-11-23 15:39:10 -0500

    first

--- client/src/routes/student-profile.jsx
+++
@@ -1,7 +0,0 @@
-export default function studentProfile() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Edición de perfil de estudiante</h2>
-      </main>
-      );
-    }
