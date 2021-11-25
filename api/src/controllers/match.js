f3c9795ae73711b9561db6097cdffb01378fbb33
davidB2ya <dbedoya0408@gmail.com>
2021-11-24 19:16:35 -0500

    Match previews

--- api/src/controllers/match.js
+++ api/src/controllers/match.js
@@ -6,11 +6,30 @@ const assignedRouter = require('express').Router()
 
 const arrayEstudentsNotAssigned = []
 const arrayMentorAvailable = []
-// const match = []
+const match = []
+const listMatches = []
+let resultInterest = 0
 
-// const Match = function{
-
-// }
+async function Match () {
+  for (let est = 0; est < arrayEstudentsNotAssigned.length; est++) {
+    for (let m = 0; m < arrayMentorAvailable.length; m++) {
+      for (let i = 0; i < 2; i++) {
+        if (
+          arrayEstudentsNotAssigned[est].interestsStudent[i].includes(
+            arrayMentorAvailable[m]
+          )
+        ) {
+          if (resultInterest === 0) {
+            resultInterest = 5
+          } else {
+            resultInterest = resultInterest + 10
+          }
+        }
+      }match.push(resultInterest)
+    }listMatches.push(match)
+  }
+  return await listMatches
+}
 
 // Get all info the student
 assignedRouter.get('/students', async (req, res, next) => {
@@ -36,7 +55,18 @@ assignedRouter.get('/students', async (req, res, next) => {
   )
 
   const usersNotAssigned = await profile
-    .find({ assignedMentor: { $regex: '.*Not assigned$' } }, { _id: 1 })
+    .find(
+      { assignedMentor: { $regex: '.*Not assigned$' } },
+      {
+        _id: 1,
+        actualAge: 1,
+        commitment: 1,
+        achievementOrientation: 1,
+        flexibility: 1,
+        assertiveCommunication: 1,
+        interestsStudent: 1
+      }
+    )
     .populate('user_id')
 
   // const array = []
@@ -51,7 +81,7 @@ assignedRouter.get('/students', async (req, res, next) => {
       }
     }
   }
-  res.json(arrayEstudentsNotAssigned)
+  res.send(usersNotAssigned)
 })
 
 // Get all info the mentor
@@ -61,15 +91,33 @@ assignedRouter.get('/mentor', async (req, res, next) => {
   }
 
   const mentorAvailable = await profile
-    .find({}, { _id: 1, numeStudents: 1, studentAssignment: 1 })
+    .find(
+      {},
+      {
+        _id: 1,
+        numeStudents: 1,
+        studentAssignment: 1,
+        interestsMentor: 1,
+        actualAge: 1,
+        commitment: 1,
+        achievementOrientation: 1,
+        flexibility: 1,
+        assertiveCommunication: 1,
+        gender: 1
+      }
+    )
     .populate('user_id')
 
-  // const arrayMentorAvailable = []
-
   for (let m = 0; m < mentorAvailable.length; m++) {
-    if (mentorAvailable[m].user_id.role === 4 & mentorAvailable[m].user_id.state === true & mentorAvailable[m].user_id.program === user.program) {
-      // console.log(mentorAvailable[m])
-      if (mentorAvailable[m].studentAssignment.length < mentorAvailable[m].numeStudents) {
+    if (
+      (mentorAvailable[m].user_id.role === 4) &
+      (mentorAvailable[m].user_id.state === true) &
+      (mentorAvailable[m].user_id.program === user.program)
+    ) {
+      if (
+        mentorAvailable[m].studentAssignment.length <
+        mentorAvailable[m].numeStudents
+      ) {
         arrayMentorAvailable.push(mentorAvailable[m])
       }
     }
@@ -79,7 +127,8 @@ assignedRouter.get('/mentor', async (req, res, next) => {
 
 // Get all info the mentor
 assignedRouter.get('/result', async (req, res, next) => {
-  res.json(arrayEstudentsNotAssigned)
+  const result = await Match()
+  res.json(result)
 })
 
 module.exports = { assignedRouter }
